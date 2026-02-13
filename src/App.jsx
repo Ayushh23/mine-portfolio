2d4ba3aec92da78fc4d9a8dc970cc715c6f317df
Ayush Raj <ayushraj23062004@gmail.com>
2026-02-14 01:45:55 +0530

    Debugging App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './components/NavBar.jsx';
+import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
 import About from './components/About.jsx';
 import Skills from './components/Skills.jsx';
